18435ddccce365e3924db105c46d4a05773b12ac
Magara Etchien N'gouan <magaraetchien@live.fr>
2023-01-22 06:08:37 +0000

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Films } from '../models/film.model';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private api:ApiService) {}
 
+  film ? : Films;
+
+  loading: boolean =false;
+
+  //le chargement des donnees au chargement de la page accueil Home initialiser les attributs d'une classe
+  
+  ngOnInit(): void {
+      this.api.CallApi().then((data)=>{
+        this.film = data;
+        this.loading =true;
+      })
+  }
 }
